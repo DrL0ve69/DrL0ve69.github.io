299cc803c8f79186d85828e60279272e71819695
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-03-13 13:00:54 -0400

    Ajout des marges, peut-être à retravailler... Ajout de l'action mailto sur le formulaire

--- scripts/constructeurProduits.js
+++ scripts/constructeurProduits.js
@@ -36,38 +36,38 @@ const listeProduits =
   },
   {
     "id":2,
-    "titre": "Produit 1",
+    "titre": "Produit 2",
     "prixUnitaire": 199.99,
     "description": "Description produit",
     "image": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/20547d52-3e1b-4c3d-b917-f0d7e0eb8bdf/custom-nike-air-force-1-low-by-you-shoes.png"
   },
   {
     "id":3,
-    "titre": "Produit 1",
+    "titre": "Produit 3",
     "prixUnitaire": 199.99,
     "description": "Description produit",
     "image": "https://images.laced.com/products/83b250a4-e848-4d2c-b36b-5efa3ea0f012.jpg?auto=format&fit=max&w=3840&q=75"
   },
   {
     "id":4,
-    "titre": "Produit 1",
+    "titre": "Produit 4",
     "prixUnitaire": 199.99,
     "description": "Description produit",
     "image": "https://images.laced.com/products/e88566e0-0051-4948-ae08-5765bbdd6db0.jpg?auto=format&fit=max&w=3840&q=75"
   },
   {
     "id":5,
-    "titre": "Produit 1",
+    "titre": "Produit 5",
     "prixUnitaire": 199.99,
     "description": "Description produit",
-    "image": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/99486859-0ff3-46b4-949b-2d16af2ad421/custom-nike-dunk-high-by-you-shoes.png"
+    "image": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/956f694e-6ab5-48c5-a47d-fef6286590a8/KOBE+IX+ELITE+%28GS%29.png"
   },
   {
     "id":6,
-    "titre": "Produit 1",
+    "titre": "Produit 6",
     "prixUnitaire": 199.99,
     "description": "Description produit",
-    "image": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/20547d52-3e1b-4c3d-b917-f0d7e0eb8bdf/custom-nike-air-force-1-low-by-you-shoes.png"
+    "image": "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c3967023-e1af-403c-bc21-4a398f9df18f/LEBRON+XXII+SN.png"
   },
 ]
 console.log(listeProduits);
