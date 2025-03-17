aa0ea3cbaa6c747673fcbe4c611e4b4524928969
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-03-17 16:11:59 -0400

    Ajout page détail

---
+++ scripts/details.js
@@ -0,0 +1,27 @@
+const selectedProductID = window.location.href.split('=')[1];
+let produitSelectionne = {};
+console.log(selectedProductID);
+
+let listeProduits = [];
+function getData()
+{
+    fetch('produits.json')
+    .then(resp => resp.json())
+    .then(produitsData => 
+    {
+        produitsData.forEach(produit => {
+            
+            listeProduits.push(produit)
+        });
+        produitSelectionne = listeProduits.find(p => p.id == selectedProductID);
+        console.log(produitSelectionne);
+        let img = document.createElement('img');
+        img.src = produitSelectionne.image;
+        img.style.width = '300px';
+        document.body.append(img);
+        console.log(img);
+    })
+}
+getData();
+
+
