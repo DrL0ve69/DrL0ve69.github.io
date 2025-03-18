3719f356ee1f4944df068d75908b6d9745374a34
phili <0758510@cegepoutaouais.qc.ca>
2025-03-18 01:16:59 -0400

    Modification page détail. Manque longue description.

--- scripts/details.js
+++ scripts/details.js
@@ -15,16 +15,28 @@ function getData()
         });
         produitSelectionne = listeProduits.find(p => p.id == selectedProductID);
         console.log(produitSelectionne);
-        let divContainer = document.createElement('div');
-        divContainer.classList.add('container');
+
+        let divImgContainer = document.querySelector('#imgPageDetail');
+        let divTxtContainer = document.querySelector('#txtPageDetail');
         let img = document.createElement('img');
         img.src = produitSelectionne.image;
-        img.style.width = '50%';
-        img.style.height = '100vh';
+        img.style.width = '100%';
+        img.style.height = '90vh';
 
-        document.body.append(divContainer);
-        divContainer.append(img);
+        divImgContainer.append(img);
         console.log(img);
+
+        let titreProduit = document.createElement('h1');
+        titreProduit.textContent = `${produitSelectionne.id}. ${produitSelectionne.titre}`;
+        divTxtContainer.prepend(titreProduit);
+
+        let descriptionProduit = document.createElement('p');
+        descriptionProduit.textContent = `${produitSelectionne.description}`;
+        divTxtContainer.append(descriptionProduit);
+
+        let prixProduit = document.createElement('p');
+        prixProduit.textContent = `${produitSelectionne.prixUnitaire} $`;
+        divTxtContainer.append(prixProduit);
     })
 }
 getData();
